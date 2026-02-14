903cd13ec0bcec2c6c134d9c9978366af9f8f4ec
danithu17 <dnprog1@gmail.com>
2026-02-14 22:07:08 +0530

    fixed1

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ public:
     std::vector<sf::RectangleShape> graphBars;
 
     AppleDashboard(const sf::Font& font) : title(font), stats(font) {
-        // Main Card Layout
         card.setSize({440.f, 180.f});
         card.setFillColor(sf::Color(255, 255, 255, 30));
         card.setOutlineThickness(1.5f);
@@ -29,7 +28,6 @@ public:
         stats.setFillColor(sf::Color(200, 200, 200));
         stats.setPosition({50.f, 480.f});
 
-        // Simple Graph Simulation
         for (int i = 0; i < 7; i++) {
             sf::RectangleShape bar({30.f, -(float)(30 + (i * 10))});
             bar.setPosition({70.f + (i * 50), 580.f});
@@ -50,21 +48,18 @@ int main() {
     sf::ContextSettings settings;
     settings.antiAliasingLevel = 8;
 
-    // SFML 3.0 Window Style
-    sf::RenderWindow window(sf::VideoMode({500, 650}), "FocusPro - Apple Experience", sf::Style::Titlebar | sf::Style::Close, settings);
+    // FIX: SFML 3.0 uses sf::State instead of styles
+    sf::RenderWindow window(sf::VideoMode({500, 650}), "FocusPro Apple Experience", sf::State::Windowed, settings);
     window.setFramerateLimit(60);
 
-    // SFML 3.0 Font Loading (Now returns a pointer or throws)
     sf::Font font;
     if (!font.openFromFile("C:/Windows/Fonts/segoeui.ttf")) return -1;
 
     AppleDashboard dashboard(font);
-
     int timeLeft = 25 * 60;
     bool isRunning = false;
     sf::Clock clock;
 
-    // UI - Main Timer Ring
     sf::CircleShape ring(130.f);
     ring.setOutlineThickness(8.f);
     ring.setOutlineColor(sf::Color(60, 60, 60));
@@ -76,7 +71,6 @@ int main() {
     timerText.setCharacterSize(70);
     timerText.setFillColor(sf::Color::White);
 
-    // Modern Button
     sf::RectangleShape btn({220.f, 60.f});
     btn.setFillColor(sf::Color(0, 122, 255));
     btn.setOrigin({110.f, 30.f});
@@ -104,14 +98,12 @@ int main() {
             }
         } else { clock.restart(); }
 
-        // Timer Text Update
         int m = timeLeft / 60;
         int s = timeLeft % 60;
         std::stringstream ss;
         ss << std::setw(2) << std::setfill('0') << m << ":" << std::setw(2) << std::setfill('0') << s;
         timerText.setString(ss.str());
         
-        // Center text in ring
         sf::FloatRect textBounds = timerText.getLocalBounds();
         timerText.setOrigin({textBounds.size.x / 2.f, textBounds.size.y / 2.f});
         timerText.setPosition({250.f, 190.f});
